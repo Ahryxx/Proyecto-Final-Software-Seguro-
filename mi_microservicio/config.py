19f32a5774d10333dcf0a839e889656f373a624e
Ahryxx <90488720+Ahryxx@users.noreply.github.com>
2025-02-04 23:42:29 -0500

    Delete mi_microservicio/config.py

--- mi_microservicio/config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-SECRET_KEY = '7Gj9@b2M'  # Debe ser la misma que en la primera API
-DBCONNECT="postgresql://postgres:npg_H9UzaGen3Zfg@ep-dark-resonance-a2kztjpx-pooler.eu-central-1.aws.neon.tech/secure?sslmode=require"
-DBCONNECTAUDIT="postgresql://postgres:npg_H9UzaGen3Zfg@ep-dark-resonance-a2kztjpx-pooler.eu-central-1.aws.neon.tech/auditoria?sslmode=require"
